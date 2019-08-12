78f794851a3d4a17f2566e8b27b0cfe735157367
Guy Lin <xup6ru03xu4@gmail.com>
2019-08-13 07:33:56 +0800

    all zero return zero pass

---
+++ StringAverageKata/StringAverage.cs
@@ -0,0 +1,10 @@
+﻿namespace StringAverageKata
+{
+    public class StringAverage
+    {
+        public int GetAverage(string numbers)
+        {
+            return 0;
+        }
+    }
+}
---
+++ StringAverageKata/StringAverageTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringAverageKata
+{
+    [TestClass]
+    public class StringAverageTest
+    {
+        [TestMethod]
+        public void All_Zero_return_Zero()
+        {
+            //arrange
+            var stringAverage = new StringAverage();
+
+            //act
+            var actual = stringAverage.GetAverage("zero zero zero zero");
+
+            //assert
+            Assert.AreEqual(0, actual);
+        }
+    }
+}
